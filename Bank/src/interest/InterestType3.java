a67c065f941410e123005760c88840b647c1619e
Patryk <patryk-smol@wp.pl>
2018-04-06 13:11:38 +0200

    InterestType3

--- Bank/src/interest/InterestType3.java
+++ Bank/src/interest/InterestType3.java
@@ -1,5 +1,39 @@
 package interest;
 
-public class InterestType3 {
+import bank.Product;
 
+public class InterestType3 extends Interest{
+
+	@Override
+	public 	double CalculateInterest(Product product) {
+			
+		
+		double Balance=product.GetBalance();
+		if(Balance<1000.0) 
+		{
+			return Balance*1.02;
+			
+		}
+		else if(Balance>=1000 && Balance<=50000) {
+			return Balance*1.025;
+
+			}
+			else  
+			{
+
+				return Balance*1.03;
+				
+
+				
+			}
+	
+		
+		
+		
+		
+		}
+	
+	
+	
+	
 }

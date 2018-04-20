df045ebe36dd996252f70804c358749728df77bc
jakub.drapiewski <jakub.drapiewski@protonmail.com>
2018-04-20 10:50:03 +0200

    Transfer command

--- Bank/src/Command/Transfer.java
+++ Bank/src/Command/Transfer.java
@@ -1,13 +1,25 @@
 package Command;
 
 import bank.Account;
+import bank.Exceptions.NotEnoughMoney;
+import bank.Operation;
+import bank.Product;
+
+import javax.xml.transform.Source;
+import java.time.LocalDateTime;
 
 public class Transfer implements Command {
+	private Account to;
+	private double amount;
+
+	public Transfer(Account to, double amount) {
+		this.to = to;
+		this.amount = amount;
+	}
 
 	@Override
 	public void execute(Account operation) {
-		// TODO Auto-generated method stub
-		
+		operation.setPayment(this.amount);
+		to.setIncome(this.amount);
 	}
-
 }

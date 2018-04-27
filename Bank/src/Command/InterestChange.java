604d09a9db7aac32faf7a4abc9583d8144be34b0
inf122097 <patryk-smol@wp.pl>
2018-04-27 12:29:59 +0200

    change interest

--- Bank/src/Command/InterestChange.java
+++ Bank/src/Command/InterestChange.java
@@ -1,12 +1,20 @@
 package Command;
 
 import bank.Account;
+import interest.Interest;
 
 public class InterestChange implements Command {
 
+	private Interest newInterest;
+	public InterestChange(Interest  newInterest){
+
+		this.newInterest=newInterest;
+
+	}
 	@Override
 	public void execute(Account operation) {
-		
+
+		operation.setInterest(newInterest);
 	}
 
 }
